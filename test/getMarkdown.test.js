477b0417425c86585c40169548acff202d4e014a
ricosmall <805163443@qq.com>
2017-06-22 20:53:19 +0800

    feat(*): add new method to get pathlist
    add new function 'getPathList' in 'getMarkdown' module; also add test case to test the method
    'getPathList'.
    
    BREAKING CHANGE: add real test

--- test/getMarkdown.test.js
+++ test/getMarkdown.test.js
@@ -1,2 +1,12 @@
 const getMarkdown = require('../lib/getMarkdown');
+const expect = require('chai').expect;
+const path = require('path');
 
+describe('read file from markdown folder', () => {
+    it('getPathList', () => {
+        var rootPath = path.resolve() + '/test/markdown';
+        var pathList = getMarkdown.getPathList(rootPath);
+        var firstFileName = pathList[0].split('/').pop();
+        expect(firstFileName).to.be.equal('test.md');
+    });
+});
